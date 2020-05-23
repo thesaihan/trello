999de2915da634d7257c435c45fc07521f927da6
Sai Han <thesaihan@gmail.com>
2020-05-23 12:04:51 +0630

    Add card member

--- src/main/java/com/thesaihan/trello/controller/CardController.java
+++ src/main/java/com/thesaihan/trello/controller/CardController.java
@@ -1,6 +1,9 @@
 package com.thesaihan.trello.controller;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +16,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.thesaihan.trello.model.Account;
 import com.thesaihan.trello.model.Card;
+import com.thesaihan.trello.repository.AccountRepository;
 import com.thesaihan.trello.repository.CardRepository;
 
 @RestController
@@ -23,6 +28,8 @@ public class CardController {
 	
 	@Autowired
 	CardRepository cardRepository;
+	@Autowired
+	AccountRepository accountRepository;
 	
 	@GetMapping
 	public List<Card> getAll() {
@@ -49,6 +56,18 @@ public class CardController {
 	@RequestMapping(value = "{id}", method = RequestMethod.DELETE)
 	public void deleteById(@PathVariable Long id) {
 		cardRepository.deleteById(id);
+	}
+
+	@PostMapping(value = "add-member")
+	public Card addMember(@RequestBody Map<String, Object> payload) {
+		Card card = cardRepository.getOne(Long.parseLong(payload.get("cardId").toString()));
+		Set<Account> members = card.getMembers();
+		if(members == null){
+			members = new HashSet<>();
+		}
+		members.add(accountRepository.getOne(payload.get("accountUsername").toString()));
+		card.setMembers(members);
+		return cardRepository.saveAndFlush(card);
 	}
 
 }
