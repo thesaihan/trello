7565927137181c24030532280ee31ba04faeeed3
Sai Han <thesaihan@gmail.com>
2020-08-11 23:48:31 +0630

    get jwt secret key in uitl

--- src/main/java/com/thesaihan/trello/jwt/JwtUtils.java
+++ src/main/java/com/thesaihan/trello/jwt/JwtUtils.java
@@ -2,11 +2,18 @@ package com.thesaihan.trello.jwt;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JwtUtils {
 
   private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
+  
+	@Value("${mm.edu.ytu.erms.jwtSecret}")
+	private String jwtSecretKey;
+	
+	@Value("${mm.edu.ytu.erms.jwtExpirationMs}")
+	private int jwtExpirationMs;
 
 }
