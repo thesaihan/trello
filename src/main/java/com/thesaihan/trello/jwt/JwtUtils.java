6b9ea2decbf5e1233f113ee2f31bada5b7daaa2d
Sai Han <thesaihan@gmail.com>
2020-08-11 23:43:23 +0630

    Add logger

--- src/main/java/com/thesaihan/trello/jwt/JwtUtils.java
+++ src/main/java/com/thesaihan/trello/jwt/JwtUtils.java
@@ -1,8 +1,12 @@
 package com.thesaihan.trello.jwt;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JwtUtils {
+
+  private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
 
 }
